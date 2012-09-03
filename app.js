10d952061b21446b432d31a906793926f7acdabb
Stephen Corwin <corwinstephen@gmail.com>
2012-09-03 16:47:54 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express')
+  , app = express()
+  , routes = require('./routes')
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , path = require('path');
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.cookieParser());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/pick_a_side', routes.pickASide);
+app.get('/set_side', routes.setSide);
+app.get('/', checkSideChosen, routes.index);
+app.post('/comment', checkSideChosen, routes.createPost);
+
+function checkSideChosen(req, res, next){
+  var side = req.cookies.side;
+  if(side == "democrat" || side == "republican") {
+    next();
+  }
+  else {
+    res.redirect('/pick_a_side'); 
+  }
+};
+
+server.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
---
+++ routes/index.js
@@ -0,0 +1,73 @@
+// Redis
+var redis = require("redis"),
+    client = redis.createClient();
+
+exports.index = function(req, res){
+	var posts_romney;
+	var posts_obama;
+
+	client.lrange('post_romney_ids', 0, 20, function(err, ids){
+		console.log("Getting posts with ids: " + ids);
+		var multi = client.multi();
+		for(var k = 0; k < ids.length; k++) {
+			multi.hgetall('post:' + ids[k]);
+		}
+		
+		multi.exec(function(err, posts){
+			posts_romney = posts;
+
+			client.lrange('post_obama_ids', 0, 20, function(err, ids){
+				console.log("Getting posts with ids: " + ids);
+				var multi = client.multi();
+				for(var k = 0; k < ids.length; k++) {
+					multi.hgetall('post:' + ids[k]);
+				}
+
+				multi.exec(function(err, posts){
+					posts_obama = posts;
+
+					res.render('index', { 
+				  	title: 'Politics',
+				  	posts_romney: posts_romney,
+				  	posts_obama: posts_obama
+				  });
+				});
+				
+			});
+
+		});
+	});
+};
+
+exports.createPost = function(req, res){
+	// AJAX
+	var person = req.body['post']['person'];
+	if(person != 'romney' && person != 'obama') { return; }
+
+	client.incr('post_id');
+	client.get('post_id', function(err, reply){
+		var post_id = reply;
+
+		// Push to list
+		client.lpush('post_' + person + '_ids', post_id);
+
+		// Create post record
+		var newPost = {};
+		newPost.person = person;
+		newPost.side = req.cookies.side;
+		newPost.content = req.body['post']['content'];
+		client.hmset("post:" + post_id, "person", newPost.person, "side", newPost.side, "content", newPost.content);
+	});
+};
+
+exports.pickASide = function(req, res) {
+	res.render('pick_a_side', {
+		title: "Pick a side"
+	});
+};
+
+exports.setSide = function(req, res){
+	var side = req.query['side'];
+	res.cookie('side', side);
+	res.redirect('/');
+};
