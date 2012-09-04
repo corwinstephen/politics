2ff2022b85c44474c3464960b87862eea1034687
Stephen Corwin <corwinstephen@gmail.com>
2012-09-03 23:16:26 -0700

    Changed some socket.io config params

--- app.js
+++ app.js
@@ -41,6 +41,11 @@ server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 io.sockets.on('connection', function (socket) {
   socket.on('new_post', function(data){
     console.log(data);
